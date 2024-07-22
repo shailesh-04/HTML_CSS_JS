4ee858ffd13a7aa8d3f0866eb0544cbf575a86d7
shailesh-04 <shailesakoli@gmail.com>
2024-07-22 20:19:26 +0530

    all create my Web using Html css and js in collage time

---
+++ Jacob/index.js
@@ -0,0 +1,18 @@
+const section = document.querySelectorAll("section");
+const navLink = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >= offset && top < offset + height) {
+      navLink.forEach(links => {
+        links.classList.remove('Active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('Active');
+      });
+    };
+  });
+}
+  
